039e157f37448ff38aacfac33d3e35146c4559fc
Muhammad Abubaker <abmasood5900@gmail.com>
2026-05-26 13:29:37 +0500

    link cloaking implemented

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,5 @@
 <?php
-
-use App\Http\Controllers\Api\DocumentationController;
 use App\Http\Controllers\Api\ShortLinkController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/docs', [DocumentationController::class, 'markdown'])->name('api.docs.redirect');
-Route::get('/docs/openapi.yaml', [DocumentationController::class, 'openapi'])->name('api.docs.openapi');
 
 Route::post('/links', [ShortLinkController::class, 'store'])->name('api.links.store');
